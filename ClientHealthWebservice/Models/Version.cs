dddd9590f4da4a8a79918bc777427e11a9c6d5c7
Anders Rødland <anders.rodland@gmail.com>
2021-01-14 14:56:19 -0600

    Update to version 2.0.1
    Update to version 2.0.1

---
+++ ClientHealthWebservice/Models/Version.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientHealthWebservice.Models
+{
+    public class Version
+    {
+        public string version { get; set; }
+    }
+}
